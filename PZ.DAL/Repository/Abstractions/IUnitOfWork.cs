a0f4d5a895740e67f858542c7e0fc002301508da
KazzModan <kklopov64@gmail.com>
2024-12-18 08:47:42 +0200

    added repository and unit of work

---
+++ PZ.DAL/Repository/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PZ.DAL.Entities;
+
+namespace PZ.DAL.Repository.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity;
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ PZ.DAL/Repository/UnitOfWork.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PZ.DAL.Repository.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PZ.DAL.Entities;
+
+namespace PZ.DAL.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+        private Dictionary<string, object> _repositories;
+        private bool _disposed;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+            _repositories = new Dictionary<string, object>();
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity
+        {
+            var type = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(Repository<>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _context);
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IRepository<TEntity>)_repositories[type];
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+    }
+}
